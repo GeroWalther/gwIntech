7bcb0ab52c7028785ed549621b7aa1d7d9e5760e
GeroWalther <gero.walther@gmail.com>
2024-02-26 19:24:45 +0100

    added visitable link to chirp

--- .next/static/webpack/webpack.2c6a88f315b42849.hot-update.js
+++ .next/static/webpack/webpack.2c6a88f315b42849.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "88a6bff0ae90e7ba"; }
+/******/ 	__webpack_require__.h = function() { return "f62e3c6f8425ef63"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
