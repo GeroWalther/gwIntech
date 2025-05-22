8071cc2dc4df958f81e54c100629137d114971cd
GeroWalther <gero.walther@gmail.com>
2025-05-22 18:36:53 +0200

    added new skills

--- src/components/SkillsLogos.js
+++ src/components/SkillsLogos.js
@@ -1,34 +1,43 @@
-import React from "react";
+import React from 'react';
 import {
   ApolloLogo,
+  AWSLogo,
   CSSLogo,
+  DockerLogo,
+  DrizzleLogo,
+  ElectronLogo,
   GQLLogo,
   HTMLLogo,
   JSLogo,
   MongoDBLogo,
   NextLogo,
   NodeLogo,
+  OpenAILogo,
   PlanetScaleLogo,
   PrismaLogo,
+  PythonLogo,
   ReactLogo,
   ReactNativeLogo,
   ReactQueryLogo,
   ReduxLogo,
   SassLogo,
+  SingleStoreLogo,
+  SupabaseLogo,
+  SwiftLogo,
   TRPCLogo,
   TSLogo,
   TWCSS,
   ZodLogo,
-} from "./Icons";
+} from './Icons';
 
 export const SkillsLogos = () => {
   return (
-    <div className="mb-8">
-      <h3 className="text-3xl py-5 mt-8 mb-6  lg:text-lg  md:py-2 ">
+    <div className='mb-8'>
+      <h3 className='text-3xl py-5 mt-8 mb-6  lg:text-lg  md:py-2 '>
         Languages, Frameworks and Technologies I am working with
       </h3>
 
-      <div className="grid w-full xs:grid-cols-6  grid-cols-10 gap-1 items-center">
+      <div className='grid w-full xs:grid-cols-6  grid-cols-10 gap-1 items-center'>
         <HTMLLogo />
         <CSSLogo />
         <JSLogo />
@@ -48,6 +57,15 @@ export const SkillsLogos = () => {
         <PrismaLogo />
         <PlanetScaleLogo />
         <MongoDBLogo />
+        <AWSLogo />
+        <SwiftLogo />
+        <PythonLogo />
+        <DockerLogo />
+        <OpenAILogo />
+        <SupabaseLogo />
+        <ElectronLogo />
+        <DrizzleLogo />
+        <SingleStoreLogo />
       </div>
     </div>
   );
