6669f0858c64f6058309cd01924a73d50444f2db
GeroWalther <gero.walther@gmail.com>
2025-05-22 15:43:29 +0200

    added https for gw-intech domain

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -8,16 +8,16 @@ export default function Document() {
         <Main />
         <NextScript />
         <script
-          src='http://51.92.83.204:7070/widget/assistant.js'
+          src='https://widget.gw-intech.com/widget/assistant.js'
           data-company='gw-intech'
           data-api-key='gw-intech-api-key-456'
-          data-api-url='http://51.92.83.204:7070/api/assistant'
+          data-api-url='https://widget.gw-intech.com/api/assistant'
           async></script>
         <link
           rel='stylesheet'
-          href='http://51.92.83.204:7070/widget/style.css'
+          href='https://widget.gw-intech.com/widget/style.css'
         />
-        {/* For Testing on localhost with locally running backend on port 7070 */}
+        {/* For Testing on localhost 3000 with locally running backend on port 7070 */}
         {/* <script
           src='http://localhost:7070/widget/assistant.js'
           data-company='gw-intech'
