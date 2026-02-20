d82e4b2af9c6db315c6e1d0fc33a20f494d5c355
Gero Walther <gero.walther@gmail.com>
2026-02-20 14:57:40 +0100

    Security desktop beta version

--- src/pages/index.js
+++ src/pages/index.js
@@ -82,6 +82,20 @@ export default function Home() {
               <Dices />
             </div>
           </div>
+          <div className='w-full flex justify-center gap-6 mt-16 md:mt-10 sm:flex-col sm:items-center sm:gap-3'>
+            <Link
+              href='https://scanner-downloads-public.s3.amazonaws.com/community/v0.1.0-beta.1/Security%20Scanner%20Pro-0.1.0-beta.1-arm64.dmg'
+              target='_blank'
+              className='text-sm font-medium text-dark/60 hover:text-dark underline underline-offset-4 transition-colors'>
+              Try Security Scanner Pro for macOS (Beta)
+            </Link>
+            <Link
+              href='https://scanner-downloads-public.s3.amazonaws.com/community/v0.1.0-beta.1/Security%20Scanner%20Pro%20Setup%200.1.0-beta.1.exe'
+              target='_blank'
+              className='text-sm font-medium text-dark/60 hover:text-dark underline underline-offset-4 transition-colors'>
+              Try Security Scanner Pro for Windows (Beta)
+            </Link>
+          </div>
         </Layout>
         <HireMe />
       </main>
